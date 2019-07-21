aae1e21b30cdd38f70960ce0df377646386e379b
romannep <neproman@gmail.com>
2019-07-21 11:34:22 +0400

    settings token renew fix

--- src/katejs-settings/lib/AppServer.js
+++ src/katejs-settings/lib/AppServer.js
@@ -8,11 +8,13 @@ const AppServer = parent => class Server extends use(parent, AppUser) {
   constructor(params) {
     super(params);
     makeEntitiesFromStructures(this.entities, structures);
-    this.publicAccessRules.push({
-      entity: 'Settings',
-      method: '__get',
-      access: true,
-    });
+    // GET required user, so it shoulud be allowed, otherwise it will cause error
+    // while token expired
+    // this.publicAccessRules.push({
+    //   entity: 'Settings',
+    //   method: '__get',
+    //   access: true,
+    // });
   }
   beforeInit() {
     if (super.beforeInit) super.beforeInit();
