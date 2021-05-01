ce87ebd357c6fbb5d21e1d34ab131fc2520c0605
romannep <neproman@gmail.com>
2021-05-01 14:59:32 +0400

    build

---
+++ build/precache-manifest.50880d776eb8dd01b7c9261174653dad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d412fc2b31a54c2f89e2c9e520babdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13a3bdb0705823d26361",
+    "url": "/static/css/2.18480872.chunk.css"
+  },
+  {
+    "revision": "13a3bdb0705823d26361",
+    "url": "/static/js/2.a7b68da9.chunk.js"
+  },
+  {
+    "revision": "d88a23cb11a3d200b9b83882bd573799",
+    "url": "/static/js/2.a7b68da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3040ec2941f4f84283",
+    "url": "/static/js/main.657b65e5.chunk.js"
+  },
+  {
+    "revision": "c77dae6f676cb28467ea",
+    "url": "/static/js/runtime-main.3bcd6b56.js"
+  },
+  {
+    "revision": "c808653c268183499ea3a1bb7e1b95cd",
+    "url": "/static/media/oktopad.c808653c.svg"
+  }
+]);
