03fb87e4623ac0e0d5192614e419533998070168
romannep <neproman@gmail.com>
2021-05-07 19:49:01 +0400

    rm debug

--- src/katejs-runtime-modules/lib/AppClient.js
+++ src/katejs-runtime-modules/lib/AppClient.js
@@ -4,16 +4,10 @@ import { AppSettings } from 'katejs-modules/lib/client';
 import { packageName } from './structure';
 
 const AppClient = parent => class Client extends use(parent, AppSettings) {
-  constructor(params) {
-    super(params);
-  }
   async afterUserInit() {
     if (super.afterUserInit) {
       await super.afterUserInit();
     }
-    await new Promise((resolve) => {
-      setTimeout(resolve,3000);
-    });
     if (this.settings) {
       const modules = this.settings.modules || [];
       for (let i = 0; i < modules.length; i++) {
